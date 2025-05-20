e3f245f755dcf57a4ca67ff398f82dd63a74a8b0
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-05-20 17:31:53 -0500

    rootlayout

---
+++ frontend/src/app/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#000',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    />
+  );
+} 
